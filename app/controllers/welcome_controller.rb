bc75ef6ed6ffe6e6bdcbde257b77a789484f9f4a
Xing <sky00000kimo@gmail.com>
2016-10-07 18:25:00 +0800

    welcome controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
 	def index
 		if current_user && current_user.nanny
-			redirect_to new_in_home_nanny_path
+			redirect_to new_nanny_path
 		end
 	end
 end
