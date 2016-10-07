0620784f2519b376862c17d2e9171996963c85e3
Xing <sky00000kimo@gmail.com>
2016-10-07 17:57:11 +0800

    byebye

--- app/controllers/nannies_controller.rb
+++ app/controllers/nannies_controller.rb
@@ -58,21 +58,57 @@ class NanniesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def nanny_params
-      params.require(:nanny).permit(:status,:license_number, :introduction, :since, :care_start_date,
-																		:long_tern, :short_tern, :nanny_type, :min_care_hour,
-      															{ :qualification_ids => [], :orther_qualification_ids => [],
-      																:zipcode_ids => [], :preference_ids => [] },
-      															:user_attributes => [:name, :nickname, :birthdate,
-      																									 :line_id, :phone, :id, :_destroy],
-      															:image_attributes => [:image, :id, :_destroy],
-      															:schedule_settings_attributes => [:available, :start_at,
-      																																:end_at, :id, :_destroy],
-      															:nanny_charges_attributes => [:amount, :id, :_destroy],
-      															:in_home_nanny_attributes => [:hope_baby_quantity, :number_of_baby_care,
-      																														 :id, :_destroy,
-      																 :location_attributes  => [:zipcode_id, :address, :id, :_destroy],
-      														  	 :images_attributes => [:image, :id, :_destroy]
-      														  ])
+      params.require(:nanny).permit(:status,
+      															:license_number,
+      															:introduction,
+      															:since,
+      															:care_start_date,
+																		:long_tern,
+																		:short_tern,
+																		:nanny_type,
+																		:min_care_hour,
+      															{ :qualification_ids => [],
+      																:orther_qualification_ids => [],
+      																:zipcode_ids => [],
+      																:preference_ids => []
+      															},
+      															:user_attributes => [	:name,
+      																									 	:nickname,
+      																									 	:birthdate,
+      																										:line_id,
+      																										:phone,
+      																										:id,
+      																										:_destroy
+      																									],
+      															:image_attributes => [ :image,
+      																										 :id,
+      																										 :_destroy
+      																									 ],
+      															:schedule_settings_attributes => [ :available,
+      																																 :start_at,
+      																																 :end_at, :id,
+      																																 :_destroy
+      																															 ],
+      															:nanny_charges_attributes => [ :amount,
+														      																 :id,
+														      																 :_destroy
+														      															 ],
+      															:in_home_nanny_attributes =>
+      																	[ :hope_baby_quantity,
+      																		:number_of_baby_care,
+      																		:id,
+      																		:_destroy,
+      																 			:location_attributes  => [  :zipcode_id,
+												      																 					:address,
+												      																 					:id,
+												      																 					:_destroy
+												      																 			 ],
+      														  	 			:images_attributes => [ :image,
+      														  	 															:id,
+      														  	 															:_destroy
+      														  	 														]
+      														  		]
+      														  )
     end
 
     def user_data
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,7 @@
 class WelcomeController < ApplicationController
 	def index
+		if current_user.nanny
+			redirect_to new_in_home_nanny_path
+		end
 	end
 end
